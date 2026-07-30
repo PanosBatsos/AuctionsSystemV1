047b44535f927f193e494e01d17bf7ea03b36a29
Panos Batsos <panagiotismpatsos@gmail.com>
2026-07-30 17:52:05 +0300

    refactor(Account): change Account logic

---
+++ services/AccountService/AuctionsSystem.AccountService.Infrastructure/Persistence/AccountDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AuctionsSystem.AccountService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AuctionsSystem.AccountService.Infrastructure.Persistence
+{
+    public class AccountDbContext : DbContext
+    {
+        public AccountDbContext(DbContextOptions<AccountDbContext> options) : base(options) { }
+
+        public DbSet<Account> Accounts => Set<Account>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AccountDbContext).Assembly);
+        }
+
+    }
+}
---
+++ services/AccountService/AuctionsSystem.AccountService.Infrastructure/Persistence/Configurations/AccountConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AuctionsSystem.AccountService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionsSystem.AccountService.Infrastructure.Persistence.Configurations
+{
+    internal class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.ToTable("Accounts");
+            builder.HasKey(account => account.Id);
+
+        }
+    }
+}
