554f68e8903497ab4f8e1cd1edb149902aac30f6
Panos Batsos <panagiotismpatsos@gmail.com>
2026-07-31 20:36:08 +0300

    feat(Account): Add repos for Account:

---
+++ services/AccountService/AuctionsSystem.AccountService.Application/Abstractions/Persistence/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using AuctionsSystem.AccountService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionsSystem.AccountService.Application.Abstractions.Persistence
+{
+    public interface IAccountRepository
+    {
+        Task AddAsync(Account account, CancellationToken cancellationToken = default);
+        Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task<bool> ExistsByUsernameAsync(string username, CancellationToken cancellationToken = default);
+        Task<bool> ExistsByPhoneNumberAsync(string phoneNumber, CancellationToken cancellationToken = default);
+        Task<bool> ExistsByIdNumberAsync(string idNumber, CancellationToken cancellationToken = default);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ services/AccountService/AuctionsSystem.AccountService.Infrastructure/Persistence/Repositories/AccountRepository.cs
@@ -0,0 +1,32 @@
+﻿using AuctionsSystem.AccountService.Application.Abstractions.Persistence;
+using AuctionsSystem.AccountService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionsSystem.AccountService.Infrastructure.Persistence.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+
+        private readonly AccountDbContext _db;
+
+        public AccountRepository(AccountDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(Account account, CancellationToken cancellationToken = default)
+        {
+            await _db.Set<Account>().AddAsync(account, cancellationToken);
+        }
+
+        
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
