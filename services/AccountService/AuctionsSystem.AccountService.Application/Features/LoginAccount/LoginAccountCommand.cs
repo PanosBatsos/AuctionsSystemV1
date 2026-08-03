397965f508249d19928a42d1c64803a26eb085de
Panos Batsos <panagiotismpatsos@gmail.com>
2026-08-03 03:46:49 +0300

    feat(Login): create initial command and validatator for login

---
+++ services/AccountService/AuctionsSystem.AccountService.Application/Features/LoginAccount/LoginAccountCommand.cs
@@ -0,0 +1,13 @@
+﻿using AuctionsSystem.AccountService.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionsSystem.AccountService.Application.Features.LoginAccount
+{
+    public record LoginAccountCommand(
+        string Email,
+        string Password
+        ) : IRequest<AuthenticationResponseDto>;
+}
---
+++ services/AccountService/AuctionsSystem.AccountService.Application/Features/LoginAccount/LoginAccountCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionsSystem.AccountService.Application.Features.LoginAccount
+{
+    public class LoginAccountCommandValidator : AbstractValidator<LoginAccountCommand>
+    {
+        public LoginAccountCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("A valid email address is required.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.");
+        }
+    }
+}
