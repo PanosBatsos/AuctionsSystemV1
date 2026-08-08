1d7eb092c1a07539e6b4f4ea239a5f21ea88fc68
Panos Batsos <panagiotismpatsos@gmail.com>
2026-08-08 15:19:48 +0300

    feat: implement Command and Validator for username update

---
+++ services/AccountService/AuctionsSystem.AccountService.Application/Features/UpdateAccount/UpdateUsername/UpdateUsernameCommand.cs
@@ -0,0 +1,11 @@
+﻿using AuctionsSystem.AccountService.Application.Abstractions.Logging;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionsSystem.AccountService.Application.Features.UpdateAccount.UpdateUsername
+{
+    public record UpdateUsernameCommand(Guid Id, string NewUsername) : IRequest, ILoggableRequest;
+    
+}
---
+++ services/AccountService/AuctionsSystem.AccountService.Application/Features/UpdateAccount/UpdateUsername/UpdateUsernameCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionsSystem.AccountService.Application.Features.UpdateAccount.UpdateUsername
+{
+    internal class UpdateUsernameCommandValidator
+    {
+    }
+}
